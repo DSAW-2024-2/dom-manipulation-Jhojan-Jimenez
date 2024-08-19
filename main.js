0e359857ddfbc5dcbe08537abcf2bad5c9622781
Jhojan Jimenez <jhojanjimene@gmail.com>
2024-08-19 15:53:48 -0500

    Transition

--- main.js
+++ main.js
@@ -1,27 +1,43 @@
-const leftbutton = document.querySelector("#left");
-const rightbutton = document.querySelector("#right");
+const leftbutton = document.querySelector("#leftbutton");
+const rightbutton = document.querySelector("#rightbutton");
 const img = document.querySelector("#img");
-const pos = document.querySelector("#pos");
+const label = document.querySelector("#label");
 const imgs = [
   "assets/image_1.jpg",
   "assets/image_2.jpeg",
   "assets/image_3.jpeg",
 ];
+
 let actualImg = 0;
 
 function disable() {
   leftbutton.disabled = actualImg === 0 ? true : false;
   rightbutton.disabled = actualImg === 2 ? true : false;
-  pos.textContent = actualImg;
+  label.childNodes.forEach((node) => {
+    if (node.id === `label-${actualImg}`) {
+      node.classList = "orange";
+    } else {
+      node.classList = "transparent";
+    }
+  });
+}
+function transition() {
+  setTimeout(() => {
+    img.src = imgs[actualImg];
+    img.classList = "fade-in";
+  }, 500);
 }
 
 leftbutton.addEventListener("click", () => {
-  img.src = imgs[actualImg - 1];
+  img.classList = "fade-out";
   actualImg--;
   disable();
+  transition();
 });
+
 rightbutton.addEventListener("click", () => {
-  img.src = imgs[actualImg + 1];
+  img.classList = "fade-out";
   actualImg++;
   disable();
+  transition();
 });
