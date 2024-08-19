b620dfcacf3f91eacca6b22c91bea62ad0574d8a
Jhojan Jimenez <jhojanjimene@gmail.com>
2024-08-19 01:24:39 -0500

    Clean

--- main.js
+++ main.js
@@ -8,32 +8,18 @@ const imgs = [
   "assets/image_3.jpeg",
 ];
 let actualImg = 0;
-pos.innerHTML = actualImg;
 
 function disable() {
-  if (actualImg === 0) {
-    leftbutton.disabled = true;
-  } else {
-    leftbutton.disabled = false;
-  }
-
-  if (actualImg === 2) {
-    rightbutton.disabled = true;
-  } else {
-    rightbutton.disabled = false;
-  }
-  pos.innerHTML = actualImg;
+  leftbutton.disabled = actualImg === 0 ? true : false;
+  rightbutton.disabled = actualImg === 3 ? true : false;
+  pos.textContent = actualImg;
 }
 
 leftbutton.addEventListener("click", () => {
-  img.src = imgs[actualImg - 1];
-  actualImg = actualImg - 1;
-  console.log(actualImg);
+  img.src = imgs[actualImg--];
   disable();
 });
 rightbutton.addEventListener("click", () => {
-  img.src = imgs[actualImg + 1];
-  actualImg = actualImg + 1;
-  console.log(actualImg);
+  img.src = imgs[actualImg++];
   disable();
 });
